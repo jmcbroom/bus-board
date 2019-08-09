3e9a4a40bdb9cc0270ed23deabb90482910fda17
Jimmy McBroom <james.mcbroom@gmail.com>
2019-08-09 18:48:50 -0400

    remove key

--- functions/smart.js
+++ functions/smart.js
@@ -2,8 +2,7 @@ let rp = require('request-promise')
 
 exports.handler = function(event, context, callback) {
   console.log(`Getting times for stop ${event.queryStringParameters.stopId}`);
-  let smartKey = `7UBBSGK5hjE3htqH8BhRzy7AU`
-  let url = `http://bustime.smartbus.org/bustime/api/v2/getpredictions?key=${smartKey}&format=json&stpid=${event.queryStringParameters.stopId}`
+  let url = `http://bustime.smartbus.org/bustime/api/v2/getpredictions?key=${process.env.SMART_KEY}&format=json&stpid=${event.queryStringParameters.stopId}`
 
   rp(url)
     .then(body => {
